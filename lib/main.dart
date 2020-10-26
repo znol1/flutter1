23ccf305ed14169ceb9d4fc85d6dc2eec4e8b110
znol1 <57703588+znol1@users.noreply.github.com>
2020-10-26 19:33:42 +0300

    test gradients added

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,15 @@ class _MyHomePageState extends State<MyHomePage> {
   final List<Note> _notes = [
     Note(id: '1', date: DateTime.now(), title: 'One', name: 'Name1'),
     Note(id: '2', date: DateTime.now(), title: 'Two', name: 'Name2'),
+    Note(id: '2', date: DateTime.now(), title: 'Two', name: 'Name2'),
+    Note(id: '2', date: DateTime.now(), title: 'Two', name: 'Name2'),
+    Note(id: '2', date: DateTime.now(), title: 'Two', name: 'Name2'),
+    Note(id: '2', date: DateTime.now(), title: 'Two', name: 'Name2'),
+    Note(id: '2', date: DateTime.now(), title: 'Two', name: 'Name2'),
+    Note(id: '2', date: DateTime.now(), title: 'Two', name: 'Name3'),
+    Note(id: '2', date: DateTime.now(), title: 'Two', name: 'Name3'),
+    Note(id: '2', date: DateTime.now(), title: 'Two', name: 'Name3'),
+    Note(id: '2', date: DateTime.now(), title: 'Two', name: 'Name3'),
   ];
 
   void _incrementCounter() {
@@ -48,19 +57,16 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        height: 500,
-        child: ListView.builder(
-          padding: EdgeInsets.all(20),
-          itemBuilder: (context, item) {
-            if (item.isOdd) return SizedBox(height: 10);
+      body: ListView.builder(
+        padding: EdgeInsets.all(20),
+        itemBuilder: (context, item) {
+          if (item.isOdd) return SizedBox(height: 5);
 
-            final index = item ~/ 2;
-            while (_notes.length > index) {
-              return _buildRow(_notes[index]);
-            }
-          },
-        ),
+          final index = item ~/ 2;
+          while (_notes.length > index) {
+            return _buildRow(_notes[index]);
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
@@ -75,10 +81,33 @@ class _MyHomePageState extends State<MyHomePage> {
       padding: EdgeInsets.all(30),
       transform: Matrix4.rotationZ(0.01),
       decoration: BoxDecoration(
-        border: Border.all(
-          width: 1.5,
-          color: Color(0xFFFF000000),
-        ),
+        gradient: (() {
+          if (element.name == 'Name1') {
+            return LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [
+                  const Color.fromRGBO(204, 43, 94, 100),
+                  const Color.fromRGBO(117, 58, 136, 100)
+                ]);
+          } else if (element.name == 'Name2') {
+            return LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [
+                  const Color.fromRGBO(252, 74, 26, 100),
+                  const Color.fromRGBO(247, 183, 51, 100)
+                ]);
+          } else if (element.name == 'Name3') {
+            return LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [
+                  const Color.fromRGBO(0, 201, 255, 100),
+                  const Color.fromRGBO(146, 254, 157, 100)
+                ]);
+          }
+        }()),
         borderRadius: BorderRadius.circular(20),
       ),
       child: Row(
